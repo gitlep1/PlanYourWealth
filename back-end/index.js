d220e1a8af0df76c32fad4f819ef788bc13301c2
gitlep1 <55455906+gitlep1@users.noreply.github.com>
2025-01-28 08:24:27 -0500

    Updated

--- back-end/index.js
+++ back-end/index.js
@@ -30,13 +30,13 @@ const corsOptions = {
   },
   optionsSuccessStatus: 200,
 };
-app.use(cors(corsOptions));
-
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
+
+app.use(cors(corsOptions));
 
 app.options("*", cors());
 
